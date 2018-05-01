2baed266b5b9f1c091a3416b9562d0ae44e48168
demxic <demxic@gmail.com>
2018-05-01 17:32:56 -0500

    feat : Create "Route" class

--- model/scheduleClasses.py
+++ model/scheduleClasses.py
@@ -14,3 +14,19 @@ class Airport(object):
 
     def __str__(self):
         return "{}".format(self.iata_code)
+
+
+class Route(object):
+    """For a given airline, represents a flight number as well as origin and destination airports"""
+
+    def __init__(self, flight_number: str, departure_airport: Airport, arrival_airport: Airport,
+                 route_id: int = None, carrier_code: str = 'AM', ) -> None:
+        self.flight_number = flight_number
+        self.departure_airport = departure_airport
+        self.arrival_airport = arrival_airport
+        self._id = route_id
+        self.carrier_code = carrier_code
+
+    def __str__(self):
+        return "{} {} {} {}".format(self.carrier_code, self.flight_number,
+                                    self.departure_airport, self.arrival_airport)
