7f096e122cc4c9e5d5cd76ae646e9d6139aa8bb0
demxic <demxic@gmail.com>
2018-05-01 19:36:39 -0500

    test : Create the "TestRoute" unittest

---
+++ tests/test_route.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from model.scheduleClasses import Route, Airport
+
+
+class TestRoute(TestCase):
+    def setUp(self):
+        self.origin = Airport('MEX')
+        self.destination = Airport('JFK')
+        self.route = Route('0403', self.origin, self.destination, 1)
+
+
+class TestInit(TestRoute):
+
+    def test_flight_number(self):
+        self.assertEqual(self.route.flight_number, '0403')
+
+    def test_departure_airport(self):
+        self.assertEqual(self.route.departure_airport, self.origin)
+
+    def test_arrival_airport(self):
+        self.assertEqual(self.route.arrival_airport, self.destination)
+
+    def test_route_id(self):
+        self.assertEqual(self.route._id, 1)
+
+    def test_carrier_code(self):
+        self.assertEqual(self.route.carrier_code, 'AM')
