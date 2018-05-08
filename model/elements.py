bc3f625dc18c899880d8092fe5f695670af93b19
demxic <demxic@gmail.com>
2018-05-07 23:52:37 -0500

    feat : Create the "DotDict" class
    Access dictionary using a dot notation

---
+++ model/elements.py
@@ -0,0 +1,11 @@
+class Dotdict(dict):
+    """Dot notation access to dictionary attributes"""
+
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, val):
+        if key in self.__dict__:
+            self.__dict__[key] = val
+        else:
+            self[key] = val
