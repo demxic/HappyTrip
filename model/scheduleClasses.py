3e11a23257e18e1d9ddcfbc295709b8250eb62fe
demxic <demxic@gmail.com>
2018-05-07 23:09:55 -0500

    feat : Create the "Trip" class

--- model/scheduleClasses.py
+++ model/scheduleClasses.py
@@ -405,3 +405,187 @@ class DutyDay(object):
             body = self.events[-1].as_robust_string(rpt, rls, 4 * '')
 
         return body
+
+
+class Trip(object):
+    """
+        A trip_match is a collection of DutyDays
+        It should be started by passing in a Trip number
+    """
+
+    def __init__(self, number: str, dated: datetime.date) -> None:
+        self.number = number
+        self.duty_days = []
+        self.dated = dated
+        self._credits = {}
+
+    @property
+    def report(self):
+        return self.duty_days[0].report
+
+    @property
+    def release(self):
+        return self.duty_days[-1].release
+
+    @property
+    def rests(self):
+        """Returns a list of all calculated rests between each duty_day"""
+        return [Duration(j.report - i.release) for i, j in zip(self.duty_days[:-1], self.duty_days[1:])]
+
+    @property
+    def layovers(self):
+        """Returns a list of all layover stations """
+        return [duty_day.events[-1].destination for duty_day in self.duty_days]
+
+    def get_elapsed_dates(self):
+        """Returns a list of dates in range [self.report, self.release]"""
+        delta = self.release.date() - self.report.date()
+        all_dates = [self.report.date() + timedelta(days=i) for i in range(delta.days + 1)]
+        return all_dates
+
+    def compute_credits(self, creditator=None):
+
+        if creditator:
+            return creditator.credits_from_trip(self)
+        else:
+            self._credits['block'] = Duration(0)
+            self._credits['dh'] = Duration(0)
+            self._credits['daily'] = Duration(0)
+            for duty_day in self.duty_days:
+                duty_day.compute_credits(creditator)
+                self._credits['block'] += duty_day._credits['block']
+                self._credits['dh'] += duty_day._credits['dh']
+                self._credits['daily'] += duty_day._credits['daily']
+            self._credits.update({'total': self._credits['block'] + self._credits['dh'],
+                                  'tafb': Duration(self.release - self.report)})
+
+    def append(self, duty_day):
+        """Simply append a duty_day"""
+        self.duty_days.append(duty_day)
+
+    def pop(self, index=-1):
+        return self.duty_days.pop(index)
+
+    def how_many_sundays(self):
+        sundays = filter(lambda date: date.isoweekday() == 7, self.get_elapsed_dates())
+        return len(list(sundays))
+
+    def save_to_db(self):
+        # TODO : Guardar report en el 1er vuelo, y rls en el último del duty_day
+        with connect() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql_config.select_or_insert_trip,
+                               (self.number, self.dated,
+                                self.number, self.dated))
+                self.id = cursor.fetchone()
+                for duty_day in self.duty_days:
+                    duty_day.save_to_db(cursor, self)
+
+    def __delitem__(self, key):
+        del self.duty_days[key]
+
+    def __getitem__(self, key):
+        try:
+            item = self.duty_days[key]
+        except:
+            item = None
+        return item
+
+    def __setitem__(self, key, value):
+        self.duty_days[key] = value
+
+    def __str__(self):
+        self.compute_credits()
+        header_template = """
+        # {0.number}                                                  CHECK IN AT {0.report:%H:%M}
+        {0.report:%d%b%Y}
+        DATE  RPT  FLIGHT DEPARTS  ARRIVES  RLS  BLK        TURN        EQ"""
+
+        body_template = """{duty_day}
+                     {destination} {rest}                   {block:0}BL {dh:0}CRD {total:0}TL {daily:0}DY"""
+
+        footer_template = """
+
+          TOTALS     {total:2}TL     {block:2}BL     {dh:2}CR           {tafb:2}TAFB"""
+
+        header = header_template.format(self)
+        body = ''
+
+        for duty_day, rest in zip(self.duty_days, self.rests):
+            rest = repr(rest)
+            body = body + body_template.format(duty_day=duty_day,
+                                               destination=duty_day.events[-1].destination,
+                                               rest=rest,
+                                               **duty_day._credits)
+        else:
+            duty_day = self.duty_days[-1]
+            body = body + body_template.format(duty_day=duty_day,
+                                               destination='    ',
+                                               rest='    ',
+                                               **duty_day._credits)
+
+        footer = footer_template.format(**self._credits)
+        return header + body + footer
+
+
+class Line(object):
+    """ Represents an ordered sequence of events for a given month"""
+
+    def __init__(self, month, year, crew_member=None):
+        self.duties = []
+        self.month = month
+        self.year = year
+        self.crewMember = crew_member
+        self._credits = {}
+
+    def append(self, duty):
+        self.duties.append(duty)
+
+    def compute_credits(self, creditator=None):
+        self._credits['block'] = Duration(0)
+        self._credits['dh'] = Duration(0)
+        self._credits['daily'] = Duration(0)
+        for duty in self.duties:
+            cr = duty.compute_credits()
+            if cr:
+                self._credits['block'] += duty._credits['block']
+                self._credits['dh'] += duty._credits['dh']
+                self._credits['daily'] += duty._credits['daily']
+
+        if creditator:
+            credits_list = creditator.credits_from_line(self)
+
+        return credits_list
+
+    def return_duty(self, dutyId):
+        """Return the corresponding duty for the given dutyId"""
+        return (duty for duty in self.duties if duty.id == dutyId)
+
+    def __delitem__(self, key):
+        del self.duties[key]
+
+    def __getitem__(self, key):
+        try:
+            item = self.duties[key]
+        except:
+            item = None
+        return item
+
+    def __setitem__(self, key, value):
+        self.duties[key] = value
+
+    def __iter__(self):
+        return iter(self.duties)
+
+    def return_duty_days(self):
+        """Turn all dutydays to a list called dd """
+        dd = []
+        for element in self.duties:
+            if isinstance(element, Trip):
+                dd.extend(element.duty_days)
+            elif isinstance(element, DutyDay):
+                dd.append(element)
+        return dd
+
+    def __str__(self):
+        return "\n".join(str(d) for d in self.duties)
