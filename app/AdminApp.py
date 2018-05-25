c7aa8887cab2a79f982c00c51a34298e00baf6ae
demxic <demxic@gmail.com>
2018-05-24 20:10:02 -0500

    feat : Create 2nd step in AdminApp to store Routes and Airports

--- app/AdminApp.py
+++ app/AdminApp.py
@@ -3,7 +3,7 @@ import sys
 
 from data.database import Database
 from data.regex import trip_RE, dutyday_RE, flights_RE
-from model.scheduleClasses import Airport, Trip
+from model.scheduleClasses import Airport, Trip, Route
 
 Database.initialise(database="orgutrip", user="postgres", password="0933", host="localhost")
 source = "C:\\Users\\Xico\\PycharmProjects\\HappyTrip\\data\\iata_tzmap.txt"
@@ -11,18 +11,20 @@ pbs_path = "C:\\Users\\Xico\\Google Drive\\Sobrecargo\\PBS\\"
 file_name = "201806 PBS todos los vuelos ESB.txt"
 datetime_format = "%d%b%Y%H:%M"
 session_airports = {}
+session_routes = {}
 
 with open(pbs_path + file_name) as fp:
     content = fp.read()
     for trip_match in trip_RE.finditer(content):
         trip_dict = trip_match.groupdict()
-        date_string = trip_dict['dated']+trip_dict['check_in']
+        date_string = trip_dict['dated'] + trip_dict['check_in']
         dtracker = datetime.strptime(date_string, datetime_format)
         trip = Trip(trip_dict['number'], dtracker.date())
 
         for dutyday_match in dutyday_RE.finditer(trip_dict['duties']):
             duty_day_dict = dutyday_match.groupdict()
             for flight_match in flights_RE.finditer(duty_day_dict['flights']):
+                # First section gets airports
                 flight_dict = flight_match.groupdict()
                 city = flight_dict['destination']
                 airport = session_airports.get(city)
@@ -30,7 +32,16 @@ with open(pbs_path + file_name) as fp:
                     airport = Airport.load_from_db_by_iata_code(city)
                 session_airports[city] = airport
 
-
-
-
-
+                # Second section store routes into DB
+                carrier_code = 'AM'
+                if 'DH' in flight_dict['name']:
+                    if flight_dict['equipment'] == 'DHD':
+                        flight_dict['equipment'] = 'EMB'
+                        carrier_code = '6D'
+                    flight_dict['name'] = flight_dict['name'][2:]
+                    print("{} flight in {} equipment".format(flight_dict['name'], flight_dict['equipment']))
+                route = Route(flight_number=flight_dict['name'],
+                              departure_airport=flight_dict['origin'],
+                              arrival_airport=flight_dict['destination'],
+                              carrier_code=carrier_code)
+                route.save_to_db()
